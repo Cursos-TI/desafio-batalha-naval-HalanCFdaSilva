421ecd2afc60279280fd7a3a0f3196fb0f9ea0b6
HalanCFdaSilva <114024550+HalanCFdaSilva@users.noreply.github.com>
2025-06-24 19:55:18 -0300

    Update batalhaNaval.c
    finalizado desafio mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int TAMANHO_LINHA = 10,TAMANHO_COLUNA = 10,tabuleiro [10][10];
 
+// FUNÇÕES TABULEIRO
 void inicializar_tabuleiro() {
     for (int i = 0; i < TAMANHO_LINHA; i++) {
         for (int j = 0; j < TAMANHO_COLUNA; j++) {
@@ -13,14 +15,14 @@ void inicializar_tabuleiro() {
 void imprimir_tabuleiro() {
     for (int i = 0; i < TAMANHO_LINHA; i++) {
         for (int j = 0; j < TAMANHO_COLUNA; j++) {
-            printf("%d\t", tabuleiro[i][j]);
+            printf("\t%d", tabuleiro[i][j]);
         }
         printf("\n\n");
     }
 }
 
 
-
+// FUNÇÕES NAVIO
 struct navio {
     int pos_x;
     int pos_y;
@@ -77,6 +79,63 @@ void generate_and_insert_navio(int pos_inic_x,int pos_inic_y,int pos_fim_x, int 
     generateNavio(&navio,pos_inic_x,pos_inic_y,pos_fim_x,pos_fim_y);
     inserir_navio(&navio);
 }
+
+// FUNÇÕES PODERES
+
+int posicao_dentro_tabuleiro(int posicao, int limite_tabuleiro) {
+    return posicao >= 0 && posicao < TAMANHO_LINHA;
+}
+
+usar_cone(int centro_x,int centro_y) {
+    int limite_x=5,limite_y=3;
+    int inic_x = centro_x-limite_x/2, inic_y= centro_y-limite_y/2 + 1;
+    int pos_efeito_x, pos_efeito_y;
+
+    for (int i = 0; i < limite_y; i++) {
+        for (int j = 0; j < limite_x; j++) {
+            if (i + j >= limite_x/2 && i+j <=limite_x/2+2*i) {
+                pos_efeito_x= inic_x + j;
+                pos_efeito_y= inic_y + i;
+                if (posicao_dentro_tabuleiro(pos_efeito_x,TAMANHO_LINHA) &&
+                    posicao_dentro_tabuleiro(pos_efeito_y, TAMANHO_COLUNA)) {
+                    tabuleiro[pos_efeito_y][pos_efeito_x] = 5;
+                }
+            }
+
+        }
+    }
+
+}
+
+void usar_cruz(int centro_x,int centro_y) {
+
+    for (int i = 0; i < TAMANHO_COLUNA; i++) {
+        for (int j = 0; j < TAMANHO_LINHA; j++) {
+            if (i == centro_y || j == centro_x)
+                tabuleiro[i][j] = 5;
+        }
+    }
+
+}
+
+int usar_octaedro(int centro_x,int centro_y) {
+    int limite_x=5,limite_y=5,metade_limite_x= limite_x/2;
+    int posicao_x, posicao_y;
+    for (int i = 0; i < limite_y; i++) {
+        for (int j = 0; j < limite_x; j++) {
+            posicao_x = centro_x-limite_x/2 + j;
+            posicao_y= centro_y-limite_y/2 + i;
+            if (posicao_dentro_tabuleiro(posicao_x, TAMANHO_LINHA) && posicao_dentro_tabuleiro(posicao_y, TAMANHO_COLUNA)) {
+                if (i<=metade_limite_x && i+j>=metade_limite_x && i+j<=metade_limite_x+2*i)
+
+                    tabuleiro[posicao_y][posicao_x] = 5;
+                else if (i>=metade_limite_x && j+2>=i && j+i <=limite_x +1)
+                    tabuleiro[posicao_y][posicao_x] = 5;
+            }
+        }
+    }
+}
+
 int main(void) {
     inicializar_tabuleiro();
 
@@ -85,8 +144,13 @@ int main(void) {
     generate_and_insert_navio(1,1,3,3);
     generate_and_insert_navio(9,9,7,7);
 
+    usar_cone(0,0);
+    usar_cruz(5,5);
+    usar_octaedro(7,7);
+
     imprimir_tabuleiro();
     system("pause");
+
 
     return 0;
 }
